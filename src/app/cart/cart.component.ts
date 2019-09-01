80f8693502dce4f646fdf5b5d7a1c7eb348a7b9d
grzegorz.klarecki <grzegorz.klarecki@gmail.com>
2019-09-01 23:58:02 +0200

    remove item animation

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -4,11 +4,32 @@ import { productList } from "../subcategory/product-list.helper";
 import { CartService } from "../services/cart.service";
 import { ICartItem } from "src/interfaces/cart-item.interface";
 import { FooterService } from "../services/footer.service";
+import {
+  trigger,
+  transition,
+  style,
+  animate,
+  keyframes
+} from "@angular/animations";
 
 @Component({
   selector: "app-cart",
   templateUrl: "./cart.component.html",
-  styleUrls: ["./cart.component.scss"]
+  styleUrls: ["./cart.component.scss"],
+  animations: [
+    trigger("removeItem", [
+      transition(":leave", [
+        style({ opacity: 1 }),
+        animate(
+          "1s cubic-bezier(0.35, 0, 0.25, 1)",
+          keyframes([
+            style({ opacity: 0, offset: 0.5 }),
+            style({ opacity: 0, height: 0, offset: 1 })
+          ])
+        )
+      ])
+    ])
+  ]
 })
 export class CartComponent implements OnInit, OnDestroy {
   cartItems: ICartItem[];
