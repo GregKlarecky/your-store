9b87b9b16fddf1ead8a148d8883d9a5d485bda3d
grzegorz.klarecki <grzegorz.klarecki@gmail.com>
2019-09-21 14:22:52 +0200

    components organization

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,34 +3,35 @@ import { NgModule } from "@angular/core";
 
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
-import { HomepageComponent } from "./homepage/homepage.component";
 import { NavbarComponent } from "./shared/navbar/navbar.component";
 import { SidemenuComponent } from "./shared/sidemenu/sidemenu.component";
 import { FooterComponent } from "./shared/footer/footer.component";
 import { BaseComponent } from "./shared/base/base.component";
-import { SubcategoryComponent } from "./subcategory/subcategory.component";
-import { CartComponent } from "./cart/cart.component";
-import { ProductDetailsComponent } from "./product-details/product-details.component";
-import { ShippingComponent } from "./shipping/shipping.component";
-import { PaymentComponent } from "./payment/payment.component";
-import { OrderComponent } from "./order/order.component";
-import { OrderCompleteComponent } from "./order-complete/order-complete.component";
 import { ReactiveFormsModule } from "@angular/forms";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
-import { ProductCardComponent } from "./product-card/product-card.component";
 import { StoreDevtoolsModule } from "@ngrx/store-devtools";
 import { StoreModule } from "@ngrx/store";
 import * as fromCart from "./store/reducers/cart.reducers";
 import { environment } from "../environments/environment";
-import { CounterComponent } from "./counter/counter.component";
 import { NgbModule } from "@ng-bootstrap/ng-bootstrap";
-import { AddToCartComponent } from "./add-to-cart/add-to-cart.component";
-import { CartItemComponent } from "./cart-item/cart-item.component";
-import { SafeHTMLPipe } from './pipes/safe-html.pipe';
-import { AddressFormComponent } from './shared/address-form/address-form.component';
-import { CartSummaryComponent } from './shared/cart-summary/cart-summary.component';
-import { DeliveryFormComponent } from './shared/delivery-form/delivery-form.component';
-import { DropDownMenuComponent } from './drop-down-menu/drop-down-menu.component';
+import { SafeHTMLPipe } from "./pipes/safe-html.pipe";
+import { AddressFormComponent } from "./shared/address-form/address-form.component";
+import { CartSummaryComponent } from "./shared/cart-summary/cart-summary.component";
+import { DeliveryFormComponent } from "./shared/delivery-form/delivery-form.component";
+import { HomepageComponent } from "./components/homepage/homepage.component";
+import { SubcategoryComponent } from "./components/subcategory/subcategory.component";
+import { CartComponent } from "./components/cart/cart.component";
+import { ProductDetailsComponent } from "./components/product-details/product-details.component";
+import { ShippingComponent } from "./components/shipping/shipping.component";
+import { PaymentComponent } from "./components/payment/payment.component";
+import { OrderComponent } from "./components/order/order.component";
+import { OrderCompleteComponent } from "./components/order-complete/order-complete.component";
+import { ProductCardComponent } from "./components/product-card/product-card.component";
+import { CounterComponent } from "./components/counter/counter.component";
+import { CartItemComponent } from "./components/cart-item/cart-item.component";
+import { AddToCartComponent } from "./components/add-to-cart/add-to-cart.component";
+import { DropDownMenuComponent } from "./components/drop-down-menu/drop-down-menu.component";
+import { FiltersComponent } from "./components/filters/filters.component";
 
 @NgModule({
   declarations: [
@@ -55,7 +56,8 @@ import { DropDownMenuComponent } from './drop-down-menu/drop-down-menu.component
     AddressFormComponent,
     CartSummaryComponent,
     DeliveryFormComponent,
-    DropDownMenuComponent
+    DropDownMenuComponent,
+    FiltersComponent
   ],
   imports: [
     BrowserModule,
